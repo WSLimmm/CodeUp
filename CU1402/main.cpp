5c7402b04f92dd7fb56e4bdd192073b316549b85
WSLImmm <wslim8256@gmail.com>
2024-01-15 11:55:16 +0900

    CodeUp

---
+++ CU1402/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, input;
+	cin >> n;
+
+	int arr[1000];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> input;
+		arr[i] = input;
+	}
+
+	for (int j = n - 1; j >= 0; j--)
+		cout << arr[j] << " ";
+
+	return 0;
+}
