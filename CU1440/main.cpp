7f67a08d167ef9a78885ffa8d1fcb777dd4c1f60
WSLImmm <wslim8256@gmail.com>
2024-01-23 20:13:37 +0900

    CodeUp

---
+++ CU1440/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, input, arr[1000];
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> input;
+		arr[i] = input;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << i + 1 << ": ";
+
+		for (int j = 0; j < n; j++)
+		{
+			if (i == j)
+				continue;
+
+			if (arr[i] < arr[j])
+				cout << "< ";
+			else if (arr[i] == arr[j])
+				cout << "= ";
+			else
+				cout << "> ";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
