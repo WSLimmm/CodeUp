9e1c7a23293067ff3252e8933c32388868fb5dfb
WSLImmm <wslim8256@gmail.com>
2024-02-20 15:04:06 +0900

    CodeUp

--- CU1442/main.cpp
+++ CU1442/main.cpp
@@ -1,28 +1,46 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h> 
+#include <iostream> 
+using namespace std;
 
 int a[10001];
-int n, i, j, temp, min;
+int n, temp;
  
-int main() {
-    scanf("%d", &n);
-    for (i = 1; i <= n; i++)
-        scanf("%d", &a[i]);
-    for (i = 1; i < n; i++) {
-        min = i;
-        for (j = i + 1; j <= n; j++) {
+int main() 
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
 
+    for (int i = 0; i < n - 1; i++) 
+    {
+        int min = i;
+
+        // 최솟값을 찾는 과정
+        for (int j = i + 1; j < n; j++) 
+        {
             if (a[min] > a[j])
             {
                 min = j;
             }
         }
+
+        // 최솟값을 정렬되지 않은 데이터의 첫 번째 위치의 데이터와 교체
         temp = a[i];
         a[i] = a[min];
         a[min] = temp;
+
+        // 회차마다 배열을 출력
+        cout << i + 1 << "회차: [ ";
+        for (int k = 0; k < n; k++)
+            cout << a[k] << " ";
+
+        cout << "], 최솟값: " << a[i] << endl << endl;
     }
-    for (i = 1; i <= n; i++)
-        printf("%d\n", a[i]);
+
+    cout << "정렬 후: [ ";
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+
+    cout << "]" << endl;
 
     return 0;
 }
